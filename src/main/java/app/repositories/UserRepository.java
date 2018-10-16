9118734bccab858a5726ffbf44182ef4e912ef9e
siimkkoger <siimkkoger@gmail.com>
2018-10-16 19:25:42 +0300

    MySQL configuration and README.md.
    The folder db_test can be removed with it's content, but it's there for a little reference if you need it in the beginning.

--- src/main/java/app/repositories/UserRepository.java
+++ src/main/java/app/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package app.repositories;
 
 import app.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserRepository extends CrudRepository<User, Integer>, UserRepositoryCustom {
     User findByEmail(String email);
 
