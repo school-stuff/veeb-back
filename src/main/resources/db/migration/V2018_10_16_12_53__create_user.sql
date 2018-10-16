9305ca26ae56af0768e123a906cba6557f15c7f2
Agnes Saik <agnes.saik@gmail.com>
2018-10-16 13:41:48 +0300

    flyway implementation with demo user table

---
+++ src/main/resources/db/migration/V2018_10_16_12_53__create_user.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user
+(
+  id           INT                                NOT NULL
+    PRIMARY KEY,
+  email        VARCHAR(255)                       NOT NULL,
+  password     VARCHAR(255)                       NOT NULL,
+  first_name   VARCHAR(255)                       NULL,
+  last_name    VARCHAR(255)                       NULL,
+  day_of_birth DATE                               NULL,
+  is_trainer   BIT DEFAULT b'0'                   NOT NULL,
+  created_at   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted_at   DATETIME                           NULL
+)
+  ENGINE = MyISAM;
